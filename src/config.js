d18fff262e0eac35122aa2b889a051151f61c6e5
Nai <kmt@Mac-kSnn.local>
2024-04-24 21:49:08 +0700

    remove login file

--- src/config.js
+++ src/config.js
@@ -2,23 +2,18 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-
-import "firebase/auth" 
-import firebase from "firebase/compat/app";
-
 // Your web app's Firebase configuration
-const firebaseConfig = firebase.initializeApp({
+const firebaseConfig = {
   apiKey: "AIzaSyB5G4duXHnwRHgEmN8NCnLH12XAnAw2hC4",
   authDomain: "one-stop-sf221.firebaseapp.com",
   projectId: "one-stop-sf221",
   storageBucket: "one-stop-sf221.appspot.com",
   messagingSenderId: "161329322918",
   appId: "1:161329322918:web:42007a4fed438244358d67"
-});
+};
 
 // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-const db = getFirestore(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 export {db};
-export default firebaseConfig;
--- src/index.js
+++ src/index.js
@@ -4,130 +4,98 @@ import './index.css';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.css";
 
-// import Home from './appView/Home';
-// import LibraryServiceRoomMat from './appView/LibraryServiceRoomMat';
-// import Financial from './appView/Financial';
-// import Page404 from './appView/Page404';
-// import RelatedPersons from './appView/RelatedPersons';
-// import Transport from './appView/Transport';
-// import MentalHealthSupport from './appView/MentalHealthSupport';
-// import PhysicalHealthSupport from './appView/PhysicalHealthSupport';
-// import OccupationSupport from './appView/OccupationSupport';
-// import Consumption from './appView/Consumption';
-// import Accommodation from './appView/Accommodation';
-// import OverviewSupport from './appView/OverviewSupport';
-// import SchedulePlanning from './appView/SchedulePlanning';
-// import RegOverview from './appView/RegOverview';
-// import AllPageLink from './appView/AllPagesLink';
-// import AllSportService from './appView/AllSportService';
+import Home from './appView/Home';
+import LibraryServiceRoomMat from './appView/LibraryServiceRoomMat';
+import Financial from './appView/Financial';
+import Page404 from './appView/Page404';
+import RelatedPersons from './appView/RelatedPersons';
+import Transport from './appView/Transport';
+import MentalHealthSupport from './appView/MentalHealthSupport';
+import PhysicalHealthSupport from './appView/PhysicalHealthSupport';
+import OccupationSupport from './appView/OccupationSupport';
+import Consumption from './appView/Consumption';
+import Accommodation from './appView/Accommodation';
+import OverviewSupport from './appView/OverviewSupport';
+import SchedulePlanning from './appView/SchedulePlanning';
+import RegOverview from './appView/RegOverview';
+import AllPageLink from './appView/AllPagesLink';
+import AllSportService from './appView/AllSportService';
 
 
-
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <Home />
-//   },
-//   {
-//     path: '/Relate-Persons',
-//     element: <RelatedPersons />
-//   },
-//   {
-//     path: "/LibraryService",
-//     element: <LibraryServiceRoomMat />
-//   },
-//   {
-//     path: "/Financial",
-//     element: <Financial />
-//   },
-//   {
-//     path: '/AllSportService',
-//     element: <AllSportService />
-//   },
-//   {
-//     path: "/transport",
-//     element: <Transport />
-//   },
-//   {
-//     path: "/overviewSupport",
-//     element: <OverviewSupport />
-//   },
-//   {
-//     path: '/accommodation',
-//     element: <Accommodation />
-//   },
-//   {
-//     path: "/physical-health-support",
-//     element: <PhysicalHealthSupport />
-//   },
-//   {
-//     path: "/mental-health-support",
-//     element: <MentalHealthSupport />
-//   },
-//   {
-//     path: "/consumption",
-//     element: <Consumption />
-//   },
-//   {
-//     path: "/occupation",
-//     element: <OccupationSupport />
-//   },
-//   {
-//     path: "/SchedulePlanning",
-//     element: <SchedulePlanning />
-//   },
-//   {
-//     path: "/reg-overview",
-//     element: <RegOverview />
-//   },
-//   {
-//     path: "/all-pages",
-//     element: <AllPageLink />
-//   },
-//   {
-//     path: "*",
-//     element: <Page404 />
-//   }
-
-// ]);
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <RouterProvider router={router} />
-//   </React.StrictMode>
-
-// );
-
-
-// test lgoin system
-import Home from './testLogin/Home';
-import Auth, { AuthProvider } from './testLogin/Auth';
-import Dashboard from './testLogin/Dashboard';
-import Login from './testLogin/Login';
-import SignUp from './testLogin/SignUp';
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <Home />
   },
   {
-    path: '/dashboard',
-    element: <Dashboard />
+    path: '/Relate-Persons',
+    element: <RelatedPersons />
   },
   {
-    path: '/login',
-    element: <Login />
+    path: "/LibraryService",
+    element: <LibraryServiceRoomMat />
   },
   {
-    path: '/signup',
-    element: <SignUp />
+    path: "/Financial",
+    element: <Financial />
+  },
+  {
+    path: '/AllSportService',
+    element: <AllSportService />
+  },
+  {
+    path: "/transport",
+    element: <Transport />
+  },
+  {
+    path: "/overviewSupport",
+    element: <OverviewSupport />
+  },
+  {
+    path: '/accommodation',
+    element: <Accommodation />
+  },
+  {
+    path: "/physical-health-support",
+    element: <PhysicalHealthSupport />
+  },
+  {
+    path: "/mental-health-support",
+    element: <MentalHealthSupport />
+  },
+  {
+    path: "/consumption",
+    element: <Consumption />
+  },
+  {
+    path: "/occupation",
+    element: <OccupationSupport />
+  },
+  {
+    path: "/SchedulePlanning",
+    element: <SchedulePlanning />
+  },
+  {
+    path: "/reg-overview",
+    element: <RegOverview />
+  },
+  {
+    path: "/all-pages",
+    element: <AllPageLink />
+  },
+  {
+    path: "*",
+    element: <Page404 />
   }
-])
+
+]);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
+
 );
+
